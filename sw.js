1ee4b005c0b302d7bda960dbe75bcf712a09da5d
themortgagemaster01-eng <themortgagemaster01@gmail.com>
2026-06-24 16:44:04 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mjp-mortgage-v3';
+const CACHE='mjp-mortgage-v4';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
